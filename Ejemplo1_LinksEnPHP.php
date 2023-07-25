160ecb0bce39024a665603332368cc8c90703a01
JuanCumbe_Local <juanfernandocumbe@gmail.com>
2023-07-25 17:32:10 +0200

    Ejemplo 1 + gitignore

---
+++ Ejemplo1_LinksEnPHP.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+  EJEMPLO 1: CÓMO INTEGRAR HTML Y PHP EN UN MISMO FICHERO
+-->
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>EJEMPLO 1: HTML Y PHP</title>
+</head>
+
+<body>
+  <ul>
+    <!--COMENTARIO EN HTML-->
+    <li>
+      <!--ESTA LÍNEA MUESTRA "LO PRIMERO" COMO UN ELEMENTO DE UNA LISTA. LA PALABRA "PRIMERO" ES UN LINK QUE AL CLICAR NOS LLEVA AL DOCUMENTO "HOLA MUNDO.PHP"-->
+      <?php echo "Lo <a href='Hola_Mundo.php'> primero </a>"?>
+    </li>
+    <li>
+      <?php echo "Lo segundo"?>
+    </li>
+  </ul>
+</body>
+<!-- ES NECESARIO USAR LA EXTENSIÓN.PHP EN EL ARCHIVO PARA QUE EL SERVIDOR SEA CAPAZ DE DETECTAR EL CÓDIGO PHP. DICHO DE OTRA MANERA EL SERVIDOR PUEDE INTERPRETAR CÓDIGO HTML DENTRO DE UN ARCHIVO PHP. -->
+</html>
